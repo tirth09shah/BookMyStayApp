8d8895a1341054b37f43417fca6fc1d230ffefe9
tirth09shah <tirth09shah@gmail.com>
2026-03-12 09:43:46 +0530

    UC2:Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,38 @@
-public class BookMyStayApp {
+/**
+ * Abstract class representing a generic Room.
+ * Defines common properties shared by all room types.
+ *
+ * @author Tirth
+ * @version 2.1
+ */
+
+public abstract class Room {
+
+    private String roomType;
+    private int beds;
+    private double price;
+
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Price     : $" + price);
+    }
 }
