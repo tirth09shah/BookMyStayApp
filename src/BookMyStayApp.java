d79e3ff039af019f75d3a1c53a7ad59c0bbd46d1
tirth09shah <tirth09shah@gmail.com>
2026-03-12 10:19:34 +0530

    UC6:Reservation Confirmation & Room Allocation

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,31 +1,67 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
 /**
- * Reservation
+ * RoomAllocationService
  *
- * Represents a guest booking request for a specific room type.
+ * Processes booking requests and allocates rooms safely.
+ * Prevents duplicate room allocation using a Set.
  *
  * @author Tirth
- * @version 5.0
+ * @version 6.0
  */
 
-public class Reservation {
+public class RoomAllocationService {
 
-    private String guestName;
-    private String roomType;
+    private RoomInventory inventory;
+    private Map<String, Set<String>> allocatedRooms;
+    private int roomCounter = 1;
 
-    public Reservation(String guestName, String roomType) {
-        this.guestName = guestName;
-        this.roomType = roomType;
+    public RoomAllocationService(RoomInventory inventory) {
+        this.inventory = inventory;
+        allocatedRooms = new HashMap<>();
     }
 
-    public String getGuestName() {
-        return guestName;
-    }
+    public void processBookings(Queue<Reservation> bookingQueue) {
 
-    public String getRoomType() {
-        return roomType;
-    }
+        System.out.println("\nProcessing Booking Requests...\n");
 
-    public void displayReservation() {
-        System.out.println("Guest: " + guestName + " | Requested Room: " + roomType);
+        while (!bookingQueue.isEmpty()) {
+
+            Reservation request = bookingQueue.poll();
+            String roomType = request.getRoomType();
+
+            int available = inventory.getAvailability(roomType);
+
+            if (available > 0) {
+
+                // Generate unique room ID
+                String roomId = roomType.replace(" ", "") + "-" + roomCounter++;
+
+                // Store allocated room ID
+                allocatedRooms
+                        .computeIfAbsent(roomType, k -> new HashSet<>())
+                        .add(roomId);
+
+                // Update inventory
+                inventory.updateAvailability(roomType, available - 1);
+
+                System.out.println("Reservation Confirmed for "
+                        + request.getGuestName());
+                System.out.println("Room Type: " + roomType);
+                System.out.println("Assigned Room ID: " + roomId);
+                System.out.println("--------------------------------");
+
+            } else {
+
+                System.out.println("Reservation Failed for "
+                        + request.getGuestName());
+                System.out.println("No rooms available for: " + roomType);
+                System.out.println("--------------------------------");
+            }
+        }
     }
 }
